aadf20af15e9b1ea7560cb659c5d2c00e04a32f1
Devin Beeuwkes <devin.beeuwkes@algolia.com>
2020-12-09 11:15:44 +0100

    chore: remove html

--- src/check.ts
+++ src/check.ts
@@ -480,7 +480,7 @@ export class CheckRunner {
 
     let message = alert.Message
     if (alert.Link !== '') {
-      message = `${message}\nFor more details: <a href="${alert.Link}">${alert.Link}</a>`
+      message = `${message}\nFor more details: ${alert.Link}`
     }
 
     let annotation: ChecksCreateParamsOutputAnnotations = {
